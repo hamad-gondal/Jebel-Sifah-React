b68cc6e09a6e627a6ff0c7b16e49e20b56918f33
hamad-gondal <hadi.gondal@gmail.com>
2022-03-11 19:30:21 +0100

    Animation updated

--- src/components/not-found/not-found.tsx
+++ src/components/not-found/not-found.tsx
@@ -1,12 +1,19 @@
 import './not-found.scss';
-import Lottie from "lottie-react";
+import { useLottie } from "lottie-react";
 import notFound from "../../assets/404.json";
 import { Link } from 'react-router-dom';
 
 function NotFound() {
+    const options = {
+        animationData: notFound,
+        loop: true,
+        autoplay: true
+    };
+    const { View } = useLottie(options);
+
     return (
         <div className="notFound">
-            <Lottie animationData={notFound} />
+            <div className="animation">{View} </div>
             <p className='lost'>Looks like you are lost :(</p>
             <Link className='link btn' to="/">Return Home</Link>
         </div>
